ffca6c86119e9c40f2e88fd8702ebb2d11d0f5aa
tanzil <tanjilislam90@gmail.com>
2024-12-10 21:36:52 +0600

    added og cards

--- app/layout.js
+++ app/layout.js
@@ -5,63 +5,41 @@ import Head from "next/head";
 import CustomCursor from "@/app/components/CustomCursor";
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
+const seoData = {
+  title: "HI, I am Thorn Chorn",
+  author:'Thorn Chorn',
+  keywords:'next 15,tailwind css, motion,personal website, portfolio',
+  description: "A next js personal website",
+  siteName:'Next Portfolio',
+  url: "https://next-js-portfolio-web.netlify.app/",
+  image: "https://next-js-portfolio-web.netlify.app/og.png",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        {/* Global SEO */}
-        <meta name="title" content="Hi, I'm Thorn Chorn" />
-        <meta name="description" content="A next js personal website" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta charSet="UTF-8" />
-        <meta name="author" content="Thorn Chorn" />
-
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:url"
-          content="https://next-js-portfolio-web.netlify.app/"
-        />
-        <meta property="og:title" content="Hi, I'm Thorn Chorn" />
-        <meta property="og:description" content="A next js personal website" />
-        <meta
-          property="og:image"
-          content="https://next-js-portfolio-web.netlify.app/og.png"
-        />
-
-        {/* Open Graph Defaults */}
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:image"
-          content="https://next-js-portfolio-web.netlify.app/og.png"
-        />
-        <meta property="og:title" content="Hi, I'm Thorn Chorn" />
-        <meta
-          property="og:description"
-          content="Welcome to Your Website. Discover amazing features and learn more about what we offer."
-        />
-        <meta
-          property="og:url"
-          content="https://next-js-portfolio-web.netlify.app/"
-        />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:url"
-          content="https://next-js-portfolio-web.netlify.app/"
-        />
-        <meta property="twitter:title" content="Hi, I'm Thorn Chorn" />
-        <meta
-          property="twitter:description"
-          content="A next js personal website"
-        />
-        <meta
-          property="twitter:image"
-          content="https://next-js-portfolio-web.netlify.app/og.png"
-        />
-
-        {/* Favicon */}
+        <title>{seoData.title}</title>
+        <meta name="keywords" content={seoData.keywords} />
+        <meta name="author" content={seoData.author} />
+        <meta name="robots" content="index, follow" />
+        <meta name="description" content={seoData.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={seoData.title} />
+        <meta property="og:description" content={seoData.description} />
+        <meta property="og:image" content={seoData.image} />
+        <meta property="og:url" content={seoData.url} />
+        <meta property="og:site_name" content={seoData.siteName} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@thornchorn" />
+        <meta property="twitter:url" content={seoData.url} />
+        <meta name="twitter:title" content={seoData.title} />
+        <meta name="twitter:description" content={seoData.description} />
+        <meta name="twitter:image" content={seoData.image} />
       </Head>
       <body>
         <CustomCursor />
